f75c00d5d8a0d4d025921cc859dd3c9fd836a692
Maha Abduweli <mahajo@Mahas-MacBook-Pro.local>
2018-10-06 11:09:51 -0700

    added pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//Make a list of words to display
+//Select one random word at a time
+//Press any key to start
+//Number of guesses remaining
+//Display already entered letters
+//Logic of the game
+    //If user input matches any letter of the word, 
+        //display that letter with underscore
+        //add that letter to the already entered letters
+
+    //If user input does not match any letter of the word
+        //add one to the number of guesses 
+        //display that letter on the already entered letter
+        
+    //When the number of guesses remaining equals to 0
+        //Display a image of hanged man
+        //Display Game Over over the image
